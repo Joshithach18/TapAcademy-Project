bdd5b690577e062225df60833b1cbe8b4b0138c7
Joshithach18 <joshithachennamsetty@gmail.com>
2025-11-30 12:57:22 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,22 +1,15 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL;
-
-console.log("API Base URL:", baseURL); // TEMP: to confirm the build has correct value
-
 const api = axios.create({
-  baseURL: baseURL + "/api"
+  baseURL: '/api'
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
